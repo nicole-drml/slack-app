f782b08b24fc71d019a21c745819fe0afc623696
nicole-drml <doromal.cole@gmail.com>
2023-02-19 13:12:43 +0800

    edited image file path

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,5 @@
-import logoWithText from "/Users/nicoledoromal/AvionSchool/slack-app/src/assets/images/slack_with_white_text.png";
-import bodyImage from "/Users/nicoledoromal/AvionSchool/slack-app/src/assets/images/landing-body.png";
+import logoWithText from "../../assets/images/slack_with_white_text.png";
+import bodyImage from "../../assets/images/landing-body.png";
 import "./Landing.scss";
 import { useNavigate } from "react-router-dom";
 
